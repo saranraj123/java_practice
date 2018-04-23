f0409f6dee32e9126439b3ed534299e8936fa9b7
SXB8876 <SXB8876@C91007EBC642E6.amer.homedepot.com>
2018-04-22 20:03:33 -0400

    Git test class

---
+++ JavaDemo/src/com/srn1535/Git/Gitclass.java
@@ -0,0 +1,11 @@
+package com.srn1535.Git;
+
+
+public class Gitclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for testing Git");
+	}
+
+}
